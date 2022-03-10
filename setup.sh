b155760f7d163d13ff3a3d3f9c651c873d9a17c7
Antonio Filho <designerdeunhasdefinitivo@gmail.com>
2022-03-10 19:07:06 -0300

    Procfile

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"seu-email@dominio.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
-port = 8080\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
